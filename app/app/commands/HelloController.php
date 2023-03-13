fe74996b68ac30b7ce12c4d13f6155e404b6edc7
Muhammad Bilal <mbilal.dev4@gmail.com>
2023-03-14 00:56:54 +0500

    email_Configurations

--- app/app/commands/HelloController.php
+++ app/app/commands/HelloController.php
@@ -6,9 +6,14 @@
  */
 
 namespace app\commands;
+use Response;
 
+use Yii;
 use yii\console\Controller;
 use yii\console\ExitCode;
+use yii\httpclient\Client;
+use app\models\Fruit;
+use app\models\Nutrition;
 
 /**
  * This command echoes the first argument that you have entered.
@@ -25,10 +30,68 @@ class HelloController extends Controller
      * @param string $message the message to be echoed.
      * @return int Exit code
      */
-    public function actionIndex($message = 'hello world')
+   
+    public function actionIndex()
     {
-        echo $message . "\n";
-
-        return ExitCode::OK;
+        $client = new \yii\httpclient\Client();
+        $response = $client->createRequest()
+            ->setMethod('GET')
+            ->setUrl('https://fruityvice.com/api/fruit/all')
+            ->send();
+        if ($response->isOk) {
+            // echo "Response OK\n";
+            $fruits = $response->getData();
+            $newFruits = $this->getNewFruits($fruits);
+            if (!empty($newFruits)) {
+                echo "Email OK\n";
+                foreach ($newFruits as $fruit) {
+                    $checkId = $fruit['id'];
+                    $available = $this->existFruit($checkId);
+                    echo $available;
+                    echo "Emals OK\n";
+                    if(!$available){
+                        Yii::$app->db->createCommand()
+                        ->insert('fruit', [
+                            'name' => $fruit['name'],
+                            'genus' => $fruit['genus'], 
+                            'fruit_id' => $fruit['id'],
+                            'family' => $fruit['family'],
+                            'order' => $fruit['order'],
+                            'carbohydrates' => $fruit['nutritions']['carbohydrates'],
+                            'protein' => $fruit['nutritions']['protein'],
+                            'fat' => $fruit['nutritions']['fat'],
+                            'calories' => $fruit['nutritions']['calories'],
+                            'sugar' => $fruit['nutritions']['sugar']
+                        ])
+                            ->execute();
+                            $message = Yii::$app->mailer->compose()
+                            ->setTo('ahmad_4al@hotmail.com')
+                            ->setSubject('New fruits added')
+                            ->setHtmlBody($this->renderPartial('new-fruits', ['fruits' => $newFruits]));
+                        $message->send();
+                    }
+                 
+                }
+            }
+        }
     }
+    
+    private function getNewFruits($fruits)
+    {
+        $newFruits = [];
+        foreach ($fruits as $fruit) {
+            $existingFruit = Fruit::findOne(['name' => $fruit['name']]);
+            if (!$existingFruit) {
+                $newFruits[] = $fruit;
+            }
+        }
+        return $newFruits;
+    }
+    private function existFruit($fruitId)
+    {
+        $FruitId = Fruit::findOne(['id' => $fruitId]);
+        phpinfo($FruitId);
+        return $fruitId;
+    }
+    
 }
